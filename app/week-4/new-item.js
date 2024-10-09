1f371e9d34069eeee585ce8f1e845eaa3762138b
Nicholas Simard <njsimard@hotmail.ca>
2024-10-09 14:38:07 -0600

    Week 4 - Styling
    added styling to week 4

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -7,18 +7,29 @@ export default function NewItem() {
     const [quantity, setQuantity] = useState(1);
 
     const increment = () => {
-        if( quantity < 20 ) setQuantity(quantity + 1);
+        if(quantity < 20) setQuantity(quantity + 1);
     }
 
     const decrement = () => {
-        if( quantity > 1 ) setQuantity(quantity - 1)
+        if(quantity > 1) setQuantity(quantity - 1)
     }
 
+    let buttonStyle = "bg-blue-800 rounded mx-2 px-6 py-2 hover:bg-blue-500 active:bg-blue-400 disabled:bg-neutral-600";
+
+    let disableIncrementButton = false;
+    let disableDecrementButton = false;
+
+    if(quantity >= 10) disableIncrementButton = true;
+    else disableIncrementButton = false;
+
+    if(quantity <= 1) disableDecrementButton = true;
+    else disableDecrementButton = false; 
+
     return(
-        <div>
-            <p>Count: {quantity}</p>
-            <button onClick={decrement}>-</button>
-            <button onClick={increment}>+</button>
+        <div class="text-white bg-neutral-800 m-2 px-3 py-2 rounded max-w-48 border-2 border-neutral-500 text-center">
+            <p class="font-bold text-2x1 pb-4">Count: {quantity}</p>
+            <button onClick={decrement} disabled={disableDecrementButton} class={buttonStyle}>-</button>
+            <button onClick={increment} disabled={disableIncrementButton} class={buttonStyle}>+</button>
         </div>
     );
 }
