94a5d93f22812d4d6efa73f395d21e9b26249650
Nicholas Simard <njsimard@hotmail.ca>
2024-10-09 16:37:06 -0600

    Week 5 - Add name and category input
    added name and category input to a form along with the quantity selector, added submit button to submit form.

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -8,6 +8,7 @@ export default function NewItem() {
     const [quantity, setQuantity] = useState(1);
     const [category, setCategory] = useState("produce");
 
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -30,15 +31,18 @@ export default function NewItem() {
         setCategory("produce");
     }
 
+
+    const handleNameChange = (event) => setName(event.target.value);
+    const handleCategoryChange = (event) => setCategory(event.target.value);
+
+
     const increment = () => {
         if(quantity < 20) setQuantity(quantity + 1);
     }
-
     const decrement = () => {
         if(quantity > 1) setQuantity(quantity - 1)
     }
 
-    let buttonStyle = "bg-blue-800 rounded mx-2 px-6 py-2 hover:bg-blue-500 active:bg-blue-400 disabled:bg-neutral-600";
 
     let disableIncrementButton = false;
     let disableDecrementButton = false;
@@ -49,11 +53,41 @@ export default function NewItem() {
     if(quantity <= 1) disableDecrementButton = true;
     else disableDecrementButton = false; 
 
+
+    let buttonStyle = "bg-blue-800 rounded mx-2 px-6 py-2 hover:bg-blue-500 active:bg-blue-400 disabled:bg-neutral-600";
+
+
     return(
-        <div class="text-white bg-neutral-800 m-2 px-3 py-2 rounded max-w-48 border-2 border-neutral-500 text-center">
-            <p class="font-bold text-2x1 pb-4">Count: {quantity}</p>
-            <button onClick={decrement} disabled={disableDecrementButton} class={buttonStyle}>-</button>
-            <button onClick={increment} disabled={disableIncrementButton} class={buttonStyle}>+</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Item Name:</label>
+                <input type="text" required onChange={handleNameChange} value={name}></input>
+            </div>
+            <div class="text-white bg-neutral-800 m-2 px-3 py-2 rounded max-w-48 border-2 border-neutral-500 text-center">
+                <p class="font-bold text-2x1 pb-4">Count: {quantity}</p>
+                <button type="button" onClick={decrement} disabled={disableDecrementButton} class={buttonStyle}>-</button>
+                <button type="button" onClick={increment} disabled={disableIncrementButton} class={buttonStyle}>+</button>
+            </div>
+            <div>
+                <label>Category:</label>
+                <select onChange={handleCategoryChange} value={category}>
+                    <option value="produce">Produce</option>
+                    <option value="dairy">Dairy</option>
+                    <option value="bakery">Bakery</option>
+                    <option value="meat">Meat</option>
+                    <option value="frozen foods">Frozen Foods</option>
+                    <option value="canned goods">Canned Goods</option>
+                    <option value="dry goods">Dry Goods</option>
+                    <option value="beverages">Beverages</option>
+                    <option value="snacks">Snacks</option>
+                    <option value="household">Household</option>                    
+                    <option value="other">Other</option>
+                </select>
+            </div>
+            <div>
+                <button>Submit</button>
+            </div>
+        </form>
+
     );
 }
