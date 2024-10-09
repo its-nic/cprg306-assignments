7f5e1b95bf12ccfa1545916699be78752cfb2ef9
Nicholas Simard <njsimard@hotmail.ca>
2024-10-09 13:55:14 -0600

    Week 4 - State variable for new-item component
    Added quantity state variable with increment and decrement functions.

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,6 +1,19 @@
 "use client"
 
+import { useState } from "react";
+
 export default function NewItem() {
+
+    const [quantity, setQuantity] = useState(1);
+
+    const increment = () => {
+        if( quantity < 20 ) setQuantity(quantity + 1);
+    }
+
+    const decrement = () => {
+        if( quantity > 1 ) setQuantity(quantity - 1)
+    }
+
     return(
         <div>
 
