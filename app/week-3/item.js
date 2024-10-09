bdd0d64e65a45684979b1459910d3049838a7923
Nicholas Simard <njsimard@hotmail.ca>
2024-10-09 11:34:30 -0600

    Styling
    Added styling to shopping list

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
 export default function Item({name, quantity, category}) {
     return(
-        <li>
-            <h2>{name}</h2>
+        <li class="bg-neutral-800 m-2 px-3 py-2 rounded max-w-md border-2 border-neutral-500">
+            <h2 class="font-bold text-2xl pb-4">{name}</h2>
             <p>Quantity: {quantity}</p>
             <p>Category: {category}</p>
         </li>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList from "./item-list";
 
 export default function Page() {
     return(
-        <main>
-            <h1>Shopping List</h1>
+        <main class="bg-neutral-900 text-white">
+            <h1 class="text-4xl font-bold py-4 px-2">Shopping List</h1>
             <ItemList />
         </main>
     );
