733c2170d356c346dba1a521a5d35c2c5768cf70
Nicholas Simard <njsimard@hotmail.ca>
2024-10-29 18:24:55 -0600

    Week 7 - Update page.js
    added state variable for items in the json file. added event handler for adding a new item. render newitem component. pass add item event handler to newitem as a prop, and items state to item list.

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,10 +1,23 @@
+"use client"
+
+import { useState } from "react";
+import NewItem from "./new-item";
 import ItemList from "./item-list";
+import itemsData from "./items.json"
 
 export default function Page() {
+
+    const [items, setItems] = useState(itemsData);
+
+    const handleAddItem = (newItemObj) => {
+        setItems([...items, newItemObj]);
+    }
+
     return(
         <main class="bg-neutral-900 text-white">
             <h1 class="text-4xl font-bold py-4 px-2">Shopping List</h1>
-            <ItemList />
+            <NewItem onAddItem={handleAddItem} />
+            <ItemList itemList={items}/>
         </main>
     );
 }
