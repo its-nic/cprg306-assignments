82570e843907bebc4beb89a338cdbe500a3a9607
its-nic <njsimard@hotmail.ca>
2024-11-18 17:22:14 -0700

    Week 10 - Create database service
    Modified firebase.js to add cloud firestore, and created shopping list service to access cloud firestore.

---
+++ app/week-10/_services/shopping-list-service.js
@@ -0,0 +1,31 @@
+import { db } from "../_utils/firebase";
+import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+export async function getItems(userId) {
+    try {
+        const allItemsReference = collection(db, "users", userId, "items");
+        const allItemsQuery = query(allItemsReference);
+        const querySnapshotArray = await getDocs(allItemsQuery);
+    
+        let itemsArray = [];
+    
+        querySnapshotArray.forEach((docSnap) => {
+            let thisItem = {
+                id: docSnap.id,
+                ...docSnap.data()
+            }
+            itemsArray.push(thisItem);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function addItem(userId, item) {
+    try {
+        const newItemReference = collection(db, "users", userId, "items");
+        const newItemPromise = await addDoc(newItemReference, item);
+    } catch (error) {
+        console.log(error);
+    }
+}
